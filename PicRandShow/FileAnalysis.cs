8fee00bab22c10f03a3c765b6628cf75a69f36b3
home <yaitza@foxmail.com>
2017-08-15 23:21:52 +0800

    Project Init: 1.完成遍历文件夹下所有文件 2.界面可以展示图片 Requirement: 1.PictureBox展示卡顿，需要用线程解决

---
+++ PicRandShow/FileAnalysis.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PicRandShow
+{
+    class FileAnalysis
+    {
+        private string filePath;
+
+        private List<string> files = new List<string>();
+
+        public FileAnalysis(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public List<string> GetAllFile()
+        {
+            if(!Directory.Exists(this.filePath))
+            {
+                throw new DirectoryNotFoundException($"{this.filePath} Not Exists!");
+            }
+
+            this.RecursionGetFiles(this.filePath);
+            return files;
+        }
+
+        public void RecursionGetFiles(string filePath)
+        {
+            DirectoryInfo di = new DirectoryInfo(filePath);
+
+            foreach(DirectoryInfo folder in di.GetDirectories())
+            {
+                RecursionGetFiles(folder.FullName);
+            }
+
+            foreach(FileInfo file in di.GetFiles())
+            {
+                this.files.Add($"{filePath}\\{file.Name}");
+            }
+            
+        }
+    }
+}
---
+++ PicRandShow/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PicRandShow
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            this.test();
+        }
+
+        public void test()
+        {
+            String str = ConfigurationSettings.AppSettings["filePath"];
+            var t = new FileAnalysis(str);
+            var names = t.GetAllFile();
+            //this.pictureBox.Image = new System.Drawing.Bitmap(names[300]);
+
+            //foreach (string file in names)
+            //{
+            //    this.pictureBox.Image =new  System.Drawing.Bitmap(file);
+            //    Thread.Sleep(1000);
+            //}
+            ShowPictures(5, 1, names.ToArray());
+        }
+
+        public void ShowPictures(int pics, int sec, string[] names)
+        {
+            //while (true)
+            {
+                for (int i = 0; i < pics; i++)
+                {
+                    Random rd = new Random();
+                    string fileName = names[rd.Next(0, 400)];
+                    int width = this.Width;
+                    int height = this.Height;
+
+
+                    PictureBox pic = new PictureBox();
+                    pic.Size = new Size(100, 50);
+                    pic.SizeMode = PictureBoxSizeMode.AutoSize;
+                    pic.Location = new Point(rd.Next(0, height), rd.Next(0, width));
+                    Bitmap bm = new Bitmap(fileName);
+                    pic.Image = bm;
+                    this.Controls.Add(pic);
+                    //Thread th = new Thread(new ParameterizedThreadStart(PicPlay));
+                    //th.Start(names[i]);
+                    this.Refresh();
+
+                }
+
+                Thread.Sleep(1000 * 5);
+                //this.Controls.Clear();
+                this.Refresh();
+                Thread.Sleep(1000 * 3);
+            }
+
+        }
+
+        public void PicPlay(object file)
+        {
+            string fileName = (string)file;
+            int width = this.Width;
+            int height = this.Height;
+            Random rd = new Random();
+
+
+            PictureBox pic = new PictureBox();
+            pic.Size = new Size(50, 50);
+            pic.Location = new Point(rd.Next(0, height), rd.Next(0, width));
+            Bitmap bm = new Bitmap(fileName);
+            pic.Image = bm;
+            this.Controls.Add(pic);
+
+        }
+    }
+}
